d19c4682f4a3eaf31e296131cc183522c41844e3
Vladislav Kraskov <VVlKraskov@sberbank.ru>
2021-06-10 06:05:50 +0300

    asd

--- main.c
+++ main.c
@@ -5,6 +5,11 @@ int main() {
     int a;
     int b;
     scanf("%d\n%d", &a, &b);
-    printf("%d\n%d\n", a, b);
+    if (a == 1 && b == 2){
+        printf("whoops\n");
+    }
+    else {
+        printf("%d\n%d\n", a, b);
+    }
     return 0;
 }
